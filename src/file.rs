da844dea89881f5a401f72069b0c974a22295b9c
Spxg <itsme@spxg.me>
2020-05-23 23:31:05 +0800

    update 'read many clusters' function

--- src/file.rs
+++ src/file.rs
@@ -35,35 +35,37 @@ impl<BASE> File<BASE>
 
         let block = if self.length % 512 == 0 { self.length / 512 } else { self.length / 512 + 1 };
 
-        let mut fat = [0; 512];
-        self.base.read(&mut fat, self.bpb.fat1(), 1).unwrap();
-        let fat = self.bpb.get_fat(&fat);
-
         if block <= 8 {
             let remain_bytes = (self.length % (8 * 512)) as usize;
             let remain_block = if remain_bytes % 512 == 0 { remain_bytes / 512 } else { remain_bytes / 512 + 1 };
             self.base.read(buf, self.bpb.offset(self.file_cluster), remain_block as u32).unwrap();
         } else {
-            self.base.read(&mut buf[0..512 * 8], self.bpb.offset(self.file_cluster), 8).unwrap();
+            let mut fat_base = [0; 512];
             let mut count = 1;
 
-            for &i in fat[self.file_cluster as usize..].iter() {
-                if i >= 0x0FFFFFF8 && i <= 0x0FFFFFFF {
-                    break;
+            for offset in 0.. {
+                self.base.read(&mut fat_base, self.bpb.fat1() + offset * 512, 1).unwrap();
+                let fat = self.bpb.get_fat(&fat_base);
+                self.base.read(&mut buf[0..512 * 8], self.bpb.offset(self.file_cluster), 8).unwrap();
+
+                for &i in fat[if offset == 0 { self.file_cluster as usize } else { 0 }..].iter() {
+                    if i >= 0x0FFFFFF8 && i <= 0x0FFFFFFF {
+                        return Ok(self.length as usize);
+                    }
+
+                    let start_at = count * 512 * 8 as usize;
+                    let remain_bytes = self.length as usize - (count * 512);
+                    let remain_block = if remain_bytes % (8 * 512) == 0 { remain_bytes / 512 } else { remain_bytes / 512 + 1 };
+
+                    if remain_bytes < 512 * 8 {
+                        self.base.read(&mut buf[start_at..start_at + 512 * remain_block], self.bpb.offset(i), remain_block as u32).unwrap();
+                    } else {
+                        self.base.read(&mut buf[start_at..start_at + 512 * 8],
+                                       self.bpb.offset(i), 8).unwrap();
+                    }
+
+                    count += 1;
                 }
-
-                let start_at = count * 512 * 8 as usize;
-                let remain_bytes = self.length as usize - (count * 512) ;
-                let remain_block = if remain_bytes % (8 * 512) == 0 { remain_bytes / 512 } else { remain_bytes / 512 + 1 };
-
-                if remain_bytes < 512 * 8 {
-                    self.base.read(&mut buf[start_at..start_at + 512 * remain_block], self.bpb.offset(i), remain_block as u32).unwrap();
-                } else {
-                    self.base.read(&mut buf[start_at..start_at + 512 * 8],
-                                   self.bpb.offset(i), 8).unwrap();
-                }
-
-                count += 1;
             }
         }
 
