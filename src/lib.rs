5ad76d9ef9297f7e9d25d4f1ff8828df47b05d8f
Spxg <itsme@unsafe.me>
2020-09-24 20:09:30 +0800

    rewriting

--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,71 @@
-//! # fat32
-//! This is a simple fat32 filesystem library, which is `#![no_std]` and does not use `alloc`.
-//! This is a personal project, your issues may not be resolved in time
-//!
-//! ## Support
-//! * create file or dir
-//! * read and write file
-//! * delete file or dir
-//!
-//! ## Will Support
-//! * append file
-//! * format
-//!
-//! ## How to use
-//!
-//! You need make your library implement [`BlockDevice v0.1.0` trait](https://github.com/play-stm32/block_device):
-//!
-//! ```rust
-//! pub trait BlockDevice {
-//!     type Error;
-//!     fn read(&self, buf: &mut [u8], address: u32, number_of_blocks: u32) -> Result<(), Self::Error>;
-//!     fn write(&self, buf: &[u8], address: u32, number_of_blocks: u32) -> Result<(), Self::Error>;
-//! }
-//! ```
-//!
-//! For example, I use my another library [sdio_sdhc](https://github.com/play-stm32/sdio_sdhc) to implement:
-//!
-//! ```rust
-//! impl BlockDevice for Card {
-//!    type Error = CmdError;
-//!
-//!    fn read(&self, buf: &mut [u8], address: u32, number_of_blocks: u32) -> Result<(), Self::Error> {
-//!        if number_of_blocks == 1 {
-//!            self.read_block(buf, address)?
-//!        } else {
-//!            self.read_multi_blocks(buf, address, number_of_blocks)?
-//!        }
-//!
-//!        Ok(())
-//!    }
-//!
-//!    fn write(&self, buf: &[u8], address: u32, number_of_blocks: u32) -> Result<(), Self::Error> {
-//!        if number_of_blocks == 1 {
-//!            self.write_block(buf, address)?
-//!        } else {
-//!            self.write_multi_blocks(buf, address, number_of_blocks)?
-//!        }
-//!
-//!        Ok(())
-//!    }
-//! }
-//! ```
-//!
-//! Now [sdio_sdhc](https://github.com/play-stm32/sdio_sdhc) library can support fat32 filesystem.
-//! Then, add fat32 library to your application
-//!
-//! ```
-//! # if no feature, the BUFFER_SIZE is 512 Bytes
-//! fat32 = { version = "0.1" }
-//! ```
-//!
-//! If your card block is other size, like 1024 Bytes
-//!
-//! ```
-//! fat32 = { version = "0.1", features = ["1024"] }
-//! ```
-//!
-//! Then, you can do some tests
-//!
-//! ```rust
-//! // Card from sdio_sdhc
-//! let card = Card::init().unwrap();
-//! // Volume from fat32
-//! let cont = Volume::new(card);
-//! // into root dir
-//! let root = cont.root_dir();
-//! // create file named test.txt
-//! root.create_file("test.txt").unwrap();
-//! // load file
-//! let mut file = root.load_file("test.txt").unwrap();
-//! // write buffer to file
-//! file.write(&[80; 512 * 9]).unwrap();
-//! ```
-//!
-//! If all goes well, the file was created with 4608 Bytes in root dir.
+#[cfg(test)]
+mod tests {
+    extern crate winapi;
 
-#![no_std]
-pub mod base;
-pub mod bpb;
-pub mod file;
-pub mod dir;
+    use self::winapi::ctypes::c_void;
+    use winapi::um::fileapi;
+    use std::os::raw::{c_ulong, c_long};
+    use std::ptr;
 
-#[cfg(feature = "512")]
-const BUFFER_SIZE: usize = 512;
-#[cfg(feature = "1024")]
-const BUFFER_SIZE: usize = 1024;
-#[cfg(feature = "2048")]
-const BUFFER_SIZE: usize = 2048;
-#[cfg(feature = "4069")]
-const BUFFER_SIZE: usize = 4069;
+    const GENERIC_READ: c_ulong = 1 << 31;
+    const FILE_SHARE_READ: c_ulong = 0x00000001;
+    const OPEN_EXISTING: c_ulong = 3;
+    const INVALID_HANDLE_VALUE: *mut c_void = 0xffffffffffffffff as *mut c_void;
+    const FILE_BEGIN: c_ulong = 0;
+
+    fn create_file_a(disk: &str) -> *mut c_void {
+        unsafe {
+            fileapi::CreateFileA(disk.as_ptr() as *const i8,
+                                 GENERIC_READ,
+                                 FILE_SHARE_READ,
+                                 ptr::null_mut(),
+                                 OPEN_EXISTING,
+                                 0,
+                                 ptr::null_mut())
+        }
+    }
+
+    fn set_file_pointer(handle: *mut c_void, offset: c_long) {
+        unsafe {
+            fileapi::SetFilePointer(handle,
+                                    offset,
+                                    ptr::null_mut(),
+                                    FILE_BEGIN);
+        }
+    }
+
+    fn read_file(handle: *mut c_void,
+                 buf: &mut [u8],
+                 number_of_blocks: c_ulong,
+                 number_of_bytes_read: &mut c_ulong,
+    ) -> bool {
+        let bool_int = unsafe {
+            fileapi::ReadFile(handle,
+                              buf.as_ptr() as *mut c_void,
+                              number_of_blocks * 512,
+                              number_of_bytes_read as *mut c_ulong,
+                              ptr::null_mut())
+        };
+
+        bool_int != 0
+    }
+
+    #[test]
+    fn test() {
+        let disk = "\\\\.\\F:";
+        let handle = create_file_a(disk);
+        assert_ne!(handle, INVALID_HANDLE_VALUE);
+        set_file_pointer(handle, 0);
+
+        let mut buf = [0; 512];
+        let mut len =  0;
+
+        let read_res = read_file(handle,
+                                 &mut buf,
+                                 1,
+                                 &mut len);
+        assert!(read_res);
+
+        println!("{:?}", &buf[0..len as usize]);
+    }
+}
