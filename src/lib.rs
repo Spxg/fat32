158f0fad0144dba6aa273b152656285ce058015c
Spxg <itsme@unsafe.me>
2020-10-16 15:45:09 +0800

    add BUFFER_SIZE features

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,15 @@ pub mod fat;
 
 const BUFFER_SIZE: usize = 512;
 
+#[cfg(feature = "512")]
+const BUFFER_SIZE: usize = 512;
+#[cfg(feature = "1024")]
+const BUFFER_SIZE: usize = 1024;
+#[cfg(feature = "2048")]
+const BUFFER_SIZE: usize = 2048;
+#[cfg(feature = "4096")]
+const BUFFER_SIZE: usize = 4096;
+
 #[cfg(test)]
 #[cfg(windows)]
 mod fat32 {
